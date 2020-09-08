3f3c8dfc77915f2ad9d28067d14922ef06a6e1de
古谷駿 <furuyashun@Shuns-MacBook-Pro.local>
2020-09-08 11:39:49 +0900

    add profile edit

--- src/components/Myheader.vue
+++ src/components/Myheader.vue
@@ -1,6 +1,6 @@
 <template>
   <header>
     <router-link to="/">LIFE DESIGN </router-link>
-    <router-link to="/profile">profile</router-link>
+    <router-link to="/profile">Profile</router-link>
   </header>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import firebase from "firebase";
 
 Vue.config.productionTip = false;
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB8prG_UFTj3cfj2f3WPZSMW6jwZYOrN7w",
+  authDomain: "lifedesign-418ef.firebaseapp.com",
+  databaseURL: "https://lifedesign-418ef.firebaseio.com",
+  projectId: "lifedesign-418ef",
+  storageBucket: "lifedesign-418ef.appspot.com",
+  messagingSenderId: "1063968234477",
+  appId: "1:1063968234477:web:609383bf78b61fea6425e4",
+  measurementId: "G-7QF4W20LW3"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
